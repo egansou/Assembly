6de1c176d629de4b0b0aa43afd479735e6c1e5a1
Enock Gansou <lilnoc12@gmail.com>
2019-02-28 13:35:28 -0800

    code

---
+++ fibonacci.s
@@ -0,0 +1,418 @@
+	.file	"fibonacci.c"
+	.text
+.Ltext0:
+	.section	.rodata
+.LC0:
+	.string	"%d"
+.LC1:
+	.string	"%d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.file 1 "fibonacci.c"
+	.loc 1 5 0
+	.cfi_startproc
+	pushq	%rbp
+.LCFI0:
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+.LCFI1:
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	.loc 1 5 0
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	.loc 1 8 0
+	leaq	-12(%rbp), %rax
+	movq	%rax, %rsi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	scanf
+	.loc 1 9 0
+	movl	-12(%rbp), %eax
+	movl	%eax, %edi
+	call	fib
+	movl	%eax, %esi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	.loc 1 11 0
+	movl	$0, %eax
+	.loc 1 12 0
+	movq	-8(%rbp), %rdx
+	xorq	%fs:40, %rdx
+	je	.L2
+	call	__stack_chk_fail
+.L2:
+	leave
+.LCFI2:
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.globl	fib
+	.type	fib, @function
+fib:
+.LFB1:
+	.loc 1 14 0
+	.cfi_startproc
+	pushq	%rbp
+.LCFI3:
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+.LCFI4:
+	.cfi_def_cfa_register 6
+	pushq	%rbx
+	subq	$40, %rsp
+	movl	%edi, -36(%rbp)
+	.loc 1 14 0
+	movq	%fs:40, %rax
+	movq	%rax, -24(%rbp)
+	xorl	%eax, %eax
+	.loc 1 15 0
+	cmpl	$0, -36(%rbp)
+	jne	.L4
+	.cfi_offset 3, -24
+	.loc 1 16 0
+	movl	$0, %eax
+	jmp	.L5
+.L4:
+	.loc 1 19 0
+	cmpl	$1, -36(%rbp)
+	jne	.L6
+	.loc 1 20 0
+	movl	$1, %eax
+	jmp	.L5
+.L6:
+	.loc 1 23 0
+	movl	-36(%rbp), %eax
+	subl	$1, %eax
+	movl	%eax, %edi
+	call	fib
+	movl	%eax, %ebx
+	movl	-36(%rbp), %eax
+	subl	$2, %eax
+	movl	%eax, %edi
+	call	fib
+	addl	%ebx, %eax
+.L5:
+	.loc 1 24 0
+	movq	-24(%rbp), %rdx
+	xorq	%fs:40, %rdx
+	je	.L7
+	call	__stack_chk_fail
+.L7:
+	addq	$40, %rsp
+	popq	%rbx
+	popq	%rbp
+.LCFI5:
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	fib, .-fib
+.Letext0:
+	.section	.debug_info,"",@progbits
+.Ldebug_info0:
+	.long	0xc8
+	.value	0x2
+	.long	.Ldebug_abbrev0
+	.byte	0x8
+	.uleb128 0x1
+	.long	.LASF9
+	.byte	0x1
+	.long	.LASF10
+	.long	.LASF11
+	.quad	.Ltext0
+	.quad	.Letext0
+	.long	.Ldebug_line0
+	.uleb128 0x2
+	.byte	0x8
+	.byte	0x7
+	.long	.LASF0
+	.uleb128 0x2
+	.byte	0x1
+	.byte	0x8
+	.long	.LASF1
+	.uleb128 0x2
+	.byte	0x2
+	.byte	0x7
+	.long	.LASF2
+	.uleb128 0x2
+	.byte	0x4
+	.byte	0x7
+	.long	.LASF3
+	.uleb128 0x2
+	.byte	0x1
+	.byte	0x6
+	.long	.LASF4
+	.uleb128 0x2
+	.byte	0x2
+	.byte	0x5
+	.long	.LASF5
+	.uleb128 0x3
+	.byte	0x4
+	.byte	0x5
+	.string	"int"
+	.uleb128 0x2
+	.byte	0x8
+	.byte	0x5
+	.long	.LASF6
+	.uleb128 0x2
+	.byte	0x1
+	.byte	0x6
+	.long	.LASF7
+	.uleb128 0x4
+	.byte	0x1
+	.long	.LASF8
+	.byte	0x1
+	.byte	0x5
+	.long	0x57
+	.quad	.LFB0
+	.quad	.LFE0
+	.long	.LLST0
+	.long	0x9d
+	.uleb128 0x5
+	.string	"n"
+	.byte	0x1
+	.byte	0x6
+	.long	0x57
+	.byte	0x2
+	.byte	0x91
+	.sleb128 -28
+	.byte	0
+	.uleb128 0x6
+	.byte	0x1
+	.string	"fib"
+	.byte	0x1
+	.byte	0xe
+	.byte	0x1
+	.long	0x57
+	.quad	.LFB1
+	.quad	.LFE1
+	.long	.LLST1
+	.uleb128 0x7
+	.string	"n"
+	.byte	0x1
+	.byte	0xe
+	.long	0x57
+	.byte	0x2
+	.byte	0x91
+	.sleb128 -52
+	.byte	0
+	.byte	0
+	.section	.debug_abbrev,"",@progbits
+.Ldebug_abbrev0:
+	.uleb128 0x1
+	.uleb128 0x11
+	.byte	0x1
+	.uleb128 0x25
+	.uleb128 0xe
+	.uleb128 0x13
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x1b
+	.uleb128 0xe
+	.uleb128 0x11
+	.uleb128 0x1
+	.uleb128 0x12
+	.uleb128 0x1
+	.uleb128 0x10
+	.uleb128 0x6
+	.byte	0
+	.byte	0
+	.uleb128 0x2
+	.uleb128 0x24
+	.byte	0
+	.uleb128 0xb
+	.uleb128 0xb
+	.uleb128 0x3e
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0xe
+	.byte	0
+	.byte	0
+	.uleb128 0x3
+	.uleb128 0x24
+	.byte	0
+	.uleb128 0xb
+	.uleb128 0xb
+	.uleb128 0x3e
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0x8
+	.byte	0
+	.byte	0
+	.uleb128 0x4
+	.uleb128 0x2e
+	.byte	0x1
+	.uleb128 0x3f
+	.uleb128 0xc
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x3a
+	.uleb128 0xb
+	.uleb128 0x3b
+	.uleb128 0xb
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x11
+	.uleb128 0x1
+	.uleb128 0x12
+	.uleb128 0x1
+	.uleb128 0x40
+	.uleb128 0x6
+	.uleb128 0x1
+	.uleb128 0x13
+	.byte	0
+	.byte	0
+	.uleb128 0x5
+	.uleb128 0x34
+	.byte	0
+	.uleb128 0x3
+	.uleb128 0x8
+	.uleb128 0x3a
+	.uleb128 0xb
+	.uleb128 0x3b
+	.uleb128 0xb
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x2
+	.uleb128 0xa
+	.byte	0
+	.byte	0
+	.uleb128 0x6
+	.uleb128 0x2e
+	.byte	0x1
+	.uleb128 0x3f
+	.uleb128 0xc
+	.uleb128 0x3
+	.uleb128 0x8
+	.uleb128 0x3a
+	.uleb128 0xb
+	.uleb128 0x3b
+	.uleb128 0xb
+	.uleb128 0x27
+	.uleb128 0xc
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x11
+	.uleb128 0x1
+	.uleb128 0x12
+	.uleb128 0x1
+	.uleb128 0x40
+	.uleb128 0x6
+	.byte	0
+	.byte	0
+	.uleb128 0x7
+	.uleb128 0x5
+	.byte	0
+	.uleb128 0x3
+	.uleb128 0x8
+	.uleb128 0x3a
+	.uleb128 0xb
+	.uleb128 0x3b
+	.uleb128 0xb
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x2
+	.uleb128 0xa
+	.byte	0
+	.byte	0
+	.byte	0
+	.section	.debug_loc,"",@progbits
+.Ldebug_loc0:
+.LLST0:
+	.quad	.LFB0-.Ltext0
+	.quad	.LCFI0-.Ltext0
+	.value	0x2
+	.byte	0x77
+	.sleb128 8
+	.quad	.LCFI0-.Ltext0
+	.quad	.LCFI1-.Ltext0
+	.value	0x2
+	.byte	0x77
+	.sleb128 16
+	.quad	.LCFI1-.Ltext0
+	.quad	.LCFI2-.Ltext0
+	.value	0x2
+	.byte	0x76
+	.sleb128 16
+	.quad	.LCFI2-.Ltext0
+	.quad	.LFE0-.Ltext0
+	.value	0x2
+	.byte	0x77
+	.sleb128 8
+	.quad	0
+	.quad	0
+.LLST1:
+	.quad	.LFB1-.Ltext0
+	.quad	.LCFI3-.Ltext0
+	.value	0x2
+	.byte	0x77
+	.sleb128 8
+	.quad	.LCFI3-.Ltext0
+	.quad	.LCFI4-.Ltext0
+	.value	0x2
+	.byte	0x77
+	.sleb128 16
+	.quad	.LCFI4-.Ltext0
+	.quad	.LCFI5-.Ltext0
+	.value	0x2
+	.byte	0x76
+	.sleb128 16
+	.quad	.LCFI5-.Ltext0
+	.quad	.LFE1-.Ltext0
+	.value	0x2
+	.byte	0x77
+	.sleb128 8
+	.quad	0
+	.quad	0
+	.section	.debug_aranges,"",@progbits
+	.long	0x2c
+	.value	0x2
+	.long	.Ldebug_info0
+	.byte	0x8
+	.byte	0
+	.value	0
+	.value	0
+	.quad	.Ltext0
+	.quad	.Letext0-.Ltext0
+	.quad	0
+	.quad	0
+	.section	.debug_line,"",@progbits
+.Ldebug_line0:
+	.section	.debug_str,"MS",@progbits,1
+.LASF3:
+	.string	"unsigned int"
+.LASF0:
+	.string	"long unsigned int"
+.LASF10:
+	.string	"fibonacci.c"
+.LASF4:
+	.string	"signed char"
+.LASF2:
+	.string	"short unsigned int"
+.LASF11:
+	.string	"/homes/egansou/216/project5"
+.LASF5:
+	.string	"short int"
+.LASF9:
+	.string	"GNU C 4.6.1"
+.LASF1:
+	.string	"unsigned char"
+.LASF6:
+	.string	"long int"
+.LASF8:
+	.string	"main"
+.LASF7:
+	.string	"char"
+	.ident	"GCC: (GNU) 4.6.1"
+	.section	.note.GNU-stack,"",@progbits
